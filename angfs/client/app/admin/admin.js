b8972ef86042c9786b78578edb89f5907d2b47cf
Brandon Bailey <dot_wack@yahoo.com>
2015-02-04 22:31:30 -0500

    Iiitial

---
+++ angfs/client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angfsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ angfs/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angfsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
